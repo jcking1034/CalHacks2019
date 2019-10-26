a1c4fdb4033f6dc8e3e4594ae83e400b2f1bfb07
James King <jcking1034@gmail.com>
2019-10-26 06:42:12 -0700

    Support for tags
    Also, changed to single quotes

--- src/app/views.py
+++ src/app/views.py
@@ -10,56 +10,63 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-        return render_template("about.html")
+        return render_template('about.html')
 
-# Example: http://127.0.0.1:5000/list?page=good
+
+# Example: http://127.0.0.1:5000/list?page=good&tag=visual
 @app.route('/list')
 def get_list():
     page = request.args.get('page', None)
+    tag = request.args.get('tag', None)
     if not page:
-        return "No page given"
-    return get_list_dict(page)
+        return 'No page given'
+    if not tag:
+        return 'No tag given'
+    return get_list_dict(page, tag)
 
 
-# Example: http://127.0.0.1:5000/list?page=good&name=abc&link=qqq
+# Example: http://127.0.0.1:5000/list?page=good&tag=visual&name=abc&link=qqq
 @app.route('/add')
 def add_item():
     page = request.args.get('page', None)
+    tag = request.args.get('tag', None)
     name = request.args.get('name', None)
     link = request.args.get('link', None)
     if not page:
-        return "No page given"
+        return 'No page given'
+    elif not tag:
+        return 'No tag given'
     elif not name:
-        return "No name given"
+        return 'No name given'
     elif not link:
-        return "No link given"
+        return 'No link given'
 
-    new_item = pd.DataFrame({"page": [page], "name": [name], "link": [link]})
+    new_item = pd.DataFrame({'page': [page], 'tag': [tag], 'name': [name], 'link': [link]})
     
-    data = pd.read_csv("./data.csv")
+    data = pd.read_csv('./data.csv')
     data = data.append(new_item)
-    data = data.drop_duplicates(["page", "name", "link"])
+    data = data.drop_duplicates(['page', 'tag', 'name', 'link'])
     data = data.reset_index()
-    data = data[["page", "name", "link"]]
+    data = data[['page', 'tag', 'name', 'link']]
 
-    data.to_csv("./data.csv")
+    data.to_csv('./data.csv')
 
-    return get_list_dict(page)
+    return get_list_dict(page, tag)
 
 
-def get_list_dict(page):
-    """ This function needs to get the names and links for page """
-    data = pd.read_csv("./data.csv")
-    p = data[data['page'] == page]
+def get_list_dict(page, tag):
+    ''' This function needs to get the names and links for page '''
+    data = pd.read_csv('./data.csv')
+    p = data[(data['page'] == page) & (data['tag'] == tag)]
     names = list(p['name'])
     links = list(p['link'])
 
     return {
-        "names": names,
-        "links": links,
+        'names': names,
+        'links': links,
         }
